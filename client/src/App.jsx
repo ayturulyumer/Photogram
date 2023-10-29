d46f843af1c4e331d672787e4d09e1e071ce473c
ajummer <makeudreamempty@gmail.com>
2023-10-29 12:20:53 +0200

    Add about us and footer to home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from "./components/navbar/NavBar.jsx";
 import Home from "./pages/home/home.jsx";
+import Footer from "./components/footer/Footer.jsx";
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
+      <Footer/>
     </>
   );
 }
---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,42 @@
+import { Fragment } from "react";
+import "./footer.css";
+
+export default function Footer() {
+  return (
+    <>
+      {/* Remove the container if you want to extend the Footer to full width. */}
+      <div className="container">
+        <footer className="footer">
+          <div className="footerIcons">
+            <i
+              className="navIcon fa-brands fa-square-pinterest fa-xl"
+              style={{ color: "#e60023" }}
+            ></i>
+            <i
+              className="navIcon fa-brands fa-square-facebook fa-xl"
+              style={{ color: "#1877f2" }}
+            ></i>
+            <i
+              className="navIcon fa-brands fa-square-x-twitter fa-xl"
+              style={{ color: "#000000" }}
+            ></i>
+            <i
+              className="navIcon fa-brands fa-square-instagram fa-xl"
+              style={{ color: "#e0405f" }}
+            ></i>
+            <i
+              className="navIcon fa-brands fa-linkedin fa-xl"
+              style={{ color: "#0077b5" }}
+            ></i>
+          </div>
+          {/* Copyright */}
+          <div className="footerCopyright">
+            © 2023 All rights reserved
+          </div>
+          {/* Copyright */}
+        </footer>
+      </div>
+      {/* End of .container */}
+    </>
+  );
+}
