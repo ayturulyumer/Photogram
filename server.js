068d0bcecc36b5b7dba25570ed82bd70ee92722a
ajummer <makeudreamempty@gmail.com>
2024-01-13 14:37:58 +0200

    add server to firebase

---
+++ server.js
@@ -0,0 +1,8 @@
+const server = require("./server/functions/server.js")
+
+const port = 3030;
+server.listen(port);
+console.log(
+  `Server started on port ${port}. You can make requests to http://localhost:${port}/`
+);
+console.log(`Admin panel located at http://localhost:${port}/admin`);
