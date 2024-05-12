883547406f9f7e2ac2e1d7e36985cb67584dd810
ajummer <makeudreamempty@gmail.com>
2024-05-12 10:40:32 +0300

    Change styles.css to module

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,24 +1,24 @@
-import "./navbar.css";
+import styles from "./navbar.module.css";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import AuthContext from "../../contexts/AuthContext.jsx";
 export default function NavBar() {
-  const { isAuthenticated , userAvatar , username } = useContext(AuthContext);
+  const { isAuthenticated, userAvatar, username } = useContext(AuthContext);
   return (
-    <div className="nav">
-      <div className="navLeft">
+    <div className={styles.nav}>
+      <div className={styles.navLeft}>
         <Link to={"/"} className="link">
-          <div className="logo"></div>
+          <div className={styles.logo}></div>
         </Link>
       </div>
-      <div className="navCenter">
-        <ul className="navList">
-          <li className="navListItem">
+      <div className={styles.navCenter}>
+        <ul className={styles.navList}>
+          <li className={styles.navListItem}>
             <Link to="/" className="link">
               HOME
             </Link>
           </li>
-          <li className="navListItem">
+          <li className={styles.navListItem}>
             <Link to="/photos" className="link">
               PHOTOS
             </Link>
@@ -27,27 +27,27 @@ export default function NavBar() {
           {/*If user is authenticated */}
           {isAuthenticated && (
             <>
-              <li className="navListItem">
+              <li className={styles.navListItem}>
                 <Link to="/create" className="link">
                   ADD PHOTO
                 </Link>
               </li>
-              <li className="navListItem">
-            <Link to="/explore" className="link">
-              EXPLORE
-            </Link>
-          </li>
+              <li className={styles.navListItem}>
+                <Link to="/explore" className="link">
+                  EXPLORE
+                </Link>
+              </li>
             </>
           )}
           {/*If user is  not authenticated */}
           {!isAuthenticated && (
             <>
-              <li className="navListItem">
+              <li className={styles.navListItem}>
                 <Link to="/login" className="link">
                   LOGIN
                 </Link>
               </li>
-              <li className="navListItem">
+              <li className={styles.navListItem}>
                 <Link to="/register" className="link">
                   REGISTER
                 </Link>
@@ -58,31 +58,27 @@ export default function NavBar() {
       </div>
       {/** css breaks if i put it on the first check */}
       {isAuthenticated && (
-        <div className="navRight">
-            <p>Logged in as {username}</p>
-          <ul className="wrapper">
-            <img
-              className="navImg"
-              src={userAvatar}
-              alt=""
-            />
-            <ul className="dropdown">
+        <div className={styles.navRight}>
+          <p>Logged in as {username}</p>
+          <ul className={styles.wrapper}>
+            <img className={styles.navImg} src={userAvatar} alt="navimg" />
+            <ul className={styles.dropdown}>
               <Link to="/dashboard" className="link">
-              <li className="sub-item">
-                <span className="material-symbols-outlined">grid_view</span>
-                <p>Dashboard</p>
-              </li>
+                <li className={styles.sub-item}>
+                  <span className="material-symbols-outlined">grid_view</span>
+                  <p>Dashboard</p>
+                </li>
               </Link>
               <Link to="/profile" className="link">
-                <li className="sub-item">
+                <li className={styles.sub-item}>
                   <span className="material-symbols-outlined">
                     manage_accounts
                   </span>
                   <p>Profile</p>
                 </li>
               </Link>
-              <Link to ="/logout" className="link">
-                <li className="sub-item">
+              <Link to="/logout" className="link">
+                <li className={styles.sub-item}>
                   <span className="material-symbols-outlined">logout</span>
                   <p>Logout</p>
                 </li>
